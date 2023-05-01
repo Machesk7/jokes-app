0206076e5d724fe6d645e95e722eb5e7834420e9
Jack Machesky <machesk7@msu.edu>
2023-05-01 17:12:45 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [plStandings, setPLStandings] = useState([]);
-  const [laLigaStandings, setLaLigaStandings] = useState([]);
+  const [jokes, setJokes] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        setLoading(true);
-        const [plResponse, laLigaResponse] = await Promise.all([
-          axios.get("https://api.football-data.org/v2/competitions/PL/standings", {
-            headers: { "X-Auth-Token": "60bd862c3a77463091377f3a9a575d07" }
-          }),
-          axios.get("https://api.football-data.org/v2/competitions/PD/standings", {
-            headers: { "X-Auth-Token": "60bd862c3a77463091377f3a9a575d07" }
-          })
-        ]);
-        console.log(plResponse.data);
-        console.log(laLigaResponse.data);
-        setPLStandings(plResponse.data.standings[0].table);
-        setLaLigaStandings(laLigaResponse.data.standings[0].table);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
+      setLoading(true);
+      const [chuckResponse, dadResponse] = await Promise.all([
+        axios.get('https://api.chucknorris.io/jokes/random'),
+        axios.get('https://icanhazdadjoke.com/', {
+          headers: {
+            Accept: 'application/json',
+          },
+        }),
+      ]);
+      setJokes([chuckResponse.data.value, dadResponse.data.joke]);
+      setLoading(false);
     };
 
     fetchData();
@@ -36,46 +28,11 @@ function App() {
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <>
-          <h2>Premier League Standings</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Position</th>
-                <th>Team</th>
-                <th>Points</th>
-              </tr>
-            </thead>
-            <tbody>
-              {plStandings.map((team) => (
-                <tr key={team.team.id}>
-                  <td>{team.position}</td>
-                  <td>{team.team.name}</td>
-                  <td>{team.points}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <h2>La Liga Standings</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Position</th>
-                <th>Team</th>
-                <th>Points</th>
-              </tr>
-            </thead>
-            <tbody>
-              {laLigaStandings.map((team) => (
-                <tr key={team.team.id}>
-                  <td>{team.position}</td>
-                  <td>{team.team.name}</td>
-                  <td>{team.points}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </>
+        <ul>
+          {jokes.map((joke, index) => (
+            <li key={index}>{joke}</li>
+          ))}
+        </ul>
       )}
     </div>
   );
