1e6283cad8968e7049370ce30e63c554ed213be7
Jack Machesky <machesk7@msu.edu>
2023-05-01 18:34:28 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -28,6 +28,10 @@ function JokeApp() {
     fetchData();
   }, []);
 
+  
+    
+ 
+
   return (
     <div className="container mx-auto">
       <h1 className="text-3xl font-bold text-center mb-8">Jack's Joke Generator</h1>
